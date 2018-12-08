3033d6df2b2345a57aea2b06c8aefb96b3d167eb
yugandhar.g <yugandhar.g@numerify.com>
2018-12-08 11:58:06 +0530

    selector for servers is now an attribute before was an element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css'],
   styles: [`
     h3 {
       color: aquamarine;
