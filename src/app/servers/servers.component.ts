68f5578c280cd9f64298c7a8043fbe04c61acdb0
yugandhar.g <yugandhar.g@numerify.com>
2018-12-08 09:54:40 +0530

    Html code can be directly added in TS itself

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: '<app-server></app-server>\n' +
+    '<app-server></app-server>\n',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
